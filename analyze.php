b2c253a95636e252aef4505ec995f39e0a357e5f
Ron Zacharski <raz@Ron-Zacharskis-iMac.local>
2011-01-30 20:11:23 -0500

    added index.php

--- analyze.php
+++ analyze.php
@@ -16,7 +16,7 @@
 <div id="container">
 	<div id="navigation">
 		<ul>
-		<li><a href="#">Home</a></li>
+		<li><a href="index.php">Analyzer</a></li>
 		<li><a href="#">About Us</a></li>
 		<li><a href="#">Our Products</a></li>
 		<li><a href="#">Contact Us</a></li>
@@ -175,29 +175,14 @@ $n = $_POST['gram'];
   
 ?>
     
-   
-					
-					<p>After a long hiatus from designing open source templates, I'm back! Lemonaid is a design that uses some brighter and more contemporary colours. The yellow tries to make the design look more &quot;alive&quot;. I like the way it's turned out, and enjoy the colour scheme. This design, like all of my designs, are pretty minimalistic and simple, and it's just the way I like it (Hehe). A little about me: I am a student living in Canada, and do web design as a hobby on my spare time. If you want me to design a site for you, feel free to drop me a line at <strong>web @ smallpark . org</strong>, or visit my site at <strong><a href="http://smallpark.org">smallpark.org</a></strong> (Under construction). Also, if you're looking for web hosting, you can give <a href="http://dreamhost.com">Dreamhost</a> a shot, if you use the promocode <strong>BIGDREAM</strong>, you can save 50 bucks on any plan. (And then I earn some affiliate bucks :). Now for some more Lorem Ipsum:</p>
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque tortor risus, iaculis nec, ultricies in, tempus vitae, quam. Proin semper magna a dui. Maecenas ut sem a leo molestie pretium. Nulla velit. Proin faucibus libero vitae velit. Integer mi mauris, consectetuer at, facilisis tempus, feugiat ut, nunc. Ut ut eros. Nam suscipit. Aliquam tristique arcu. <a href="#">Mauris quam.</a> Mauris vel pede id massa hendrerit convallis. Sed a orci. Morbi consequat semper metus. In hac habitasse platea dictumst. </p>
-					<h1>Lorem Ipsum Dolor Sit</h1>
-					<p>Ut magna libero, posuere ac, pharetra et, nonummy in, augue. Nullam quis ipsum. In congue, tellus ac pulvinar tempor, est enim euismod neque, at rhoncus arcu arcu eu justo. Vestibulum nec nibh. In mollis, magna ut feugiat fringilla, nunc metus tempus purus, id adipiscing ante lacus nec orci. Pellentesque vel enim.</p>
 				</div>
 				</div>
-<div id="sidebar">
-	<h1>Another Sidebar</h1>
-	<p>Vestibulum nec urna ac elit hendrerit congue. Mauris et odio non odio congue viverra. Pellentesque at mi in lacus faucibus fermentum. Integer quis odio in lorem ornare laoreet. Aenean condimentum. Maecenas vitae lacus a est ultrices nonummy. Etiam sed sapien a mi feugiat egestas. Nam sed arcu sed arcu pretium interdum. Ut quis tellus. <a href="#">Class aptent taciti sociosqu ad litora torquent per conubia </a>nostra, per inceptos hymenaeos. Etiam quis nibh. Quisque sed risus. Integer lobortis eros eu dui. Aliquam et ante. Sed nulla diam, sagittis at, nonummy sed, lobortis et, elit. Morbi rhoncus viverra tortor. Sed posuere turpis vel orci. Vivamus euismod leo vitae lectus cursus sagittis. </p>
-</div>
-<div id="extra">
-	<h1>Extra Stuff </h1>
-	<p> Praesent erat tortor, eleifend ac, mattis vel, condimentum non, ipsum. Nam pretium ante a erat. Nam justo. In ultricies volutpat diam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed libero arcu, egestas pharetra, sagittis vitae, fringilla vitae, odio. Sed volutpat libero. Vivamus malesuada pretium dolor. Donec quis eros vel pede pretium ornare. Vivamus ac eros. Phasellus sodales lectus ut dolor luctus sollicitudin. Fusce volutpat dui ac leo. Morbi urna pede, consectetuer ornare, cursus cursus, porta vitae, mi. </p>
-</div>
-<div id="footer">
-
-	<span id="design-by">Design by <a href="http://smallpark.org">Smallpark Studios<!-- Thank you for keeping this on --></a></span> 
-	  Creative Commons License.
-</div>
+<?php 
+include('bottom.html');
+?>
 </div>
 </body>
 </html>
 
+<!-- Content by Ron Zacharski. Released under Creative Commons Attribution ShareAlike 3.0 Unported License -->
 <!-- Design by Smallpark.org. Released as open source. -->
